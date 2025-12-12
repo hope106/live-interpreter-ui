705e9e2ac6eb9218298716e3b7fb495c82226d7a
웃음백만개 <106hope@gmail.com>
2025-12-12 15:06:17 +0900

    css 수정

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        zinc: {
+          750: '#3a3a3f',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
